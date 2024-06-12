6f4709c2dd31422eb2fa2d7722172a79489e591a
reggiegunawan <reggiegunawan88@gmail.com>
2024-06-12 22:00:01 +0700

    feat(header): adjust header interactivity

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,18 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function Header() {
+  const router = useRouter()
   return (
     <div style={{ background: 'linear-gradient(90.49deg, #243b6c 0.28%, #432f91 96.69%)' }}>
       <div className="flex items-center justify-between max-w-[1140px] mx-auto text-white py-3">
         <div className="flex gap-x-2">
-          <Link href="/#" className="text-2xl italic font-semibold text-white hover:text-white">
+          <div className="text-2xl italic font-semibold text-white hover:text-white" onClick={() => router.push('/')}>
             Find My Uro!
-          </Link>
+          </div>
           <span className="bg-[#f6a404] text-xs text-center px-1 text-[#432f91] self-end italic font-bold rounded-sm">BETA</span>
         </div>
         <Link href="/#">
