6118651d2d762e69c9020b0db8d0dc9c873b3003
reggiegunawan <reggiegunawan88@gmail.com>
2024-06-18 20:35:08 +0700

    feat(home): create Most Searched Cities UI section

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,15 @@
 import { ArticleSection } from './article'
 import { FindUrologist } from './find-urologist'
+import { MostSearchedCities } from './most-searched-cities'
 import { PromotionBanner } from './promotion-banner'
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-y-8">
+    <div className="flex flex-col gap-y-10">
       <FindUrologist />
       <PromotionBanner />
       <ArticleSection />
+      <MostSearchedCities />
     </div>
   )
 }
