4a5b6ef8f32d094f28f1f38368a35aac14bf6855
reggiegunawan <reggiegunawan88@gmail.com>
2024-06-29 17:17:45 +0700

    feat(homepage): update UI feedbacks

--- src/app/(main)/find-urologist/index.tsx
+++ src/app/(main)/find-urologist/index.tsx
@@ -1,27 +1,34 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import Image from 'next/image'
 import { UrologistRatingCard } from './urologist-rating-card'
 
 export function FindUrologist() {
   return (
-    <div className="flex flex-col gap-y-16 max-w-[1140px] mx-auto w-full px-5">
-      <div className="flex flex-col gap-y-5">
-        <p className="text-[40px] italic font-bold">
-          Find <span className="text-[#42328d]">Urologist</span>
-        </p>
-        <p className="text-2xl italic font-medium">
-          Vouched for by <span className="text-[#42328d]">Real Patients</span> like You
-        </p>
-      </div>
+    <div className="flex flex-col gap-y-16 max-w-[1140px] mx-auto w-full px-5 relative">
+      <div className='flex flex-col gap-y-12 relative'>
+        {/* Kidney background */}
+        <div className='hidden mobileL:absolute w-[424px] mobileL:block h-[400px] -z-10 right-[15%] -top-4'>
+          <Image alt='kidney-top' src="/assets/images/home/kidney-top-homepage.webp" style={{ clipPath: "inset(0 0 22% 0)" }} fill />
+        </div>
+        <div className="flex flex-col gap-y-2.5">
+          <p className="text-[40px] italic font-bold">
+            Find <span className="text-[#42328d]">Urologist</span>
+          </p>
+          <p className="text-2xl italic font-medium">
+            Vouched for by <span className="text-[#42328d]">Real Patients</span> like You
+          </p>
+        </div>
 
-      <div className="flex flex-col gap-y-4">
-        <span className="text-2xl italic font-medium">
-          Find Your <span className="text-[#42328d]">Best</span> Provider
-        </span>
-        <div className="grid grid-cols-1 tablet:grid-cols-3 gap-6">
-          <Input className="h-11" placeholder="Your City, State, or ZIP Code" />
-          <Input className="h-11" placeholder="Name of Urologist" />
-          <Button className="bg-[#f6a404] text-white hover:bg-orange-300 h-full">Search</Button>
+        <div className="flex flex-col gap-y-4 bg-[#ffffffc6] pb-14">
+          <span className="text-2xl italic font-medium">
+            Find Your <span className="text-[#42328d]">Best</span> Provider
+          </span>
+          <div className="grid grid-cols-1 mobileL:grid-cols-3 gap-4">
+            <Input className="h-11 text-lg bg-white" placeholder="Your City, State, or ZIP Code" />
+            <Input className="h-11 text-lg bg-white" placeholder="Name of Urologist" />
+            <Button className="bg-[#f6a404] text-white hover:bg-orange-300 h-full">Search</Button>
+          </div>
         </div>
       </div>
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,12 +8,12 @@ export default function Header() {
   const router = useRouter()
   return (
     <div style={{ background: 'linear-gradient(90.49deg, #243b6c 0.28%, #432f91 96.69%)' }}>
-      <div className="flex items-center justify-between max-w-[1140px] mx-auto text-white py-3 w-full px-5">
-        <div className="flex gap-x-2">
-          <div className="text-2xl italic font-semibold text-white hover:text-white cursor-pointer" onClick={() => router.push('/')}>
+      <div className="flex items-center justify-between max-w-[1140px] mx-auto text-white py-3 w-full px-5 cursor-pointer">
+        <div className="flex gap-x-2.5">
+          <div className="text-2xl italic font-semibold text-white hover:text-white" onClick={() => router.push('/')}>
             Find My Uro!
           </div>
-          <span className="bg-[#f6a404] text-xs text-center px-1 text-[#432f91] self-end italic font-bold rounded-sm">BETA</span>
+          <span className="bg-[#f6a404] h-fit self-end mb-1.5 text-xs text-center px-1 text-[#432f91] italic font-bold rounded-sm">BETA</span>
         </div>
         <Link href="/account/login">
           <Image className="rounded-full" alt="user-avatar" src="/assets/icons/user-avatar.webp" width={40} height={40} />
