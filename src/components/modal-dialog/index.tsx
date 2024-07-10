b871983f758c6b63025ebb0aa269970de33e20f8
reggiegunawan <reggiegunawan88@gmail.com>
2024-07-10 18:35:03 +0700

    feat(modal): create UI for vouch dialog

---
+++ src/components/modal-dialog/index.tsx
@@ -0,0 +1,27 @@
+
+import { ReactNode } from "react"
+import { Dialog, DialogContent } from "../ui/dialog"
+
+type ModalDialogProps = {
+  isOpen: boolean
+  setOpen: (value: boolean) => void
+  title: string
+  children: ReactNode
+}
+
+export function ModalDialog({ isOpen, title, setOpen, children }: ModalDialogProps) {
+  return (
+    <Dialog open={isOpen} onOpenChange={setOpen}>
+      <DialogContent className="max-w-[500px] flex flex-col p-0 justify-center bg-transparent rounded-2xl border-0 gap-0 px-6" >
+        {/* Modal Title */}
+        <div className="flex justify-center w-full text-center p-2.5 rounded-t-2xl" style={{ background: 'linear-gradient(90.49deg, #243b6c 0.28%, #432f91 96.69%)' }}>
+          <span className="text-2xl italic font-medium text-white">{title}</span>
+        </div>
+        {/* Modal Content */}
+        <div className="flex bg-white rounded-b-2xl">
+          {children}
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
