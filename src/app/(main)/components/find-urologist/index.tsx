6386ae35181a59965f07ea99f7a04e1567786519
reggiegunawan <reggiegunawan88@gmail.com>
2024-08-09 19:01:10 +0700

    feat(landing-page): integrate featured urologists

--- src/app/(main)/components/find-urologist/index.tsx
+++ src/app/(main)/components/find-urologist/index.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image'
 import { SearchSection } from './components/search-section'
-import { UrologistRatingCard } from './urologist-rating-card'
 
 export function FindUrologist() {
   return (
-    <div className="flex flex-col gap-y-16 max-w-[1140px] mx-auto w-full px-5 relative">
+    <div className="max-w-[1140px] mx-auto w-full px-5 relative">
       <div className="flex flex-col gap-y-12 relative">
         {/* Kidney background */}
         <div className="hidden mobileL:absolute w-[424px] mobileL:block h-[400px] -z-10 right-[15%] -top-4">
@@ -24,31 +23,6 @@ export function FindUrologist() {
             Find Your <span className="text-[#42328d]">Best</span> Provider
           </span>
           <SearchSection />
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-y-4">
-        <p className="text-2xl italic font-medium">
-          Featured <span className="text-[#42328d]">Urologists</span> Of The Month
-        </p>
-        <div className="grid grid-rows-1 tablet:grid-cols-3 gap-8">
-          <UrologistRatingCard />
-          <UrologistRatingCard />
-          <UrologistRatingCard />
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-y-4">
-        <div className="flex items-center justify-between">
-          <p className="text-2xl italic font-medium">
-            Highest Rated Urologists Near <span className="text-[#42328d]">(Area)</span>
-          </p>
-          <a href="urologists">see all</a>
-        </div>
-        <div className="grid grid-rows-1 tablet:grid-cols-3 gap-8">
-          <UrologistRatingCard />
-          <UrologistRatingCard />
-          <UrologistRatingCard />
         </div>
       </div>
     </div>
--- src/app/search/components/urologist-result-card/index.tsx
+++ src/app/search/components/urologist-result-card/index.tsx
@@ -1,9 +1,10 @@
 'use client'
 
+import { FeaturedUrologistLabel } from '@/app/_components/featured-urologist-label'
 import { StarGenerator } from '@/app/_components/star-generator'
 import { Button } from '@/app/_components/ui/button'
 import { UrologistSearchResult } from '@/app/_lib/definitions/search-urologist'
-import { Map, Star } from 'lucide-react'
+import { Map } from 'lucide-react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 
@@ -42,28 +43,12 @@ export function UrologistResultCard({ data, idx, setUrologistCoordinate }: Props
       <div className="flex flex-col gap-1">
         <span className="italic font-medium">{data.name}</span>
         <StarGenerator rating={data.rate} />
-        {data.is_featured && <UrologistLabel />}
+        {data.is_featured && <FeaturedUrologistLabel />}
         <Button variant="ghost" className="flex items-center justify-start gap-1 text-[#432f91] p-0 hover:bg-transparent" onClick={changeCoordinate}>
           <Map size={18} />
           <span>See on map</span>
         </Button>
       </div>
-    </div>
-  )
-}
-
-// Composing component
-// TODO: determine later whether we need this or not
-type UrologistLabelProps = {
-  text: string
-  bgColor: string
-}
-
-function UrologistLabel() {
-  return (
-    <div className="flex gap-1 items-center bg-[#FFB800] text-white text-xs rounded-lg p-[5px] w-fit">
-      <Star size="12" fill="#fff" strokeWidth={0} />
-      <span>Featured</span>
     </div>
   )
 }
