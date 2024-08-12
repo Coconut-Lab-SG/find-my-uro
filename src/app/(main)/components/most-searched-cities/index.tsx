bd9477a46be58270be8da1a27edd68f3de74bd50
reggiegunawan <reggiegunawan88@gmail.com>
2024-08-12 14:24:25 +0700

    feat(most-searched-cities): click and redirect to search page

--- src/app/(main)/components/most-searched-cities/index.tsx
+++ src/app/(main)/components/most-searched-cities/index.tsx
@@ -1,4 +1,6 @@
+import { MostSearchedCityType } from '@/app/_lib/definitions/region'
 import { getMostSearchedCities } from '@/app/_lib/services/landing-page/most-searched-cities'
+import Link from 'next/link'
 
 export async function MostSearchedCities() {
   const data = await getMostSearchedCities()
@@ -6,13 +8,21 @@ export async function MostSearchedCities() {
   return (
     <div className="flex flex-col gap-y-2 max-w-[1140px] mx-auto w-full px-5">
       <p className="text-2xl italic font-medium">Most Searched Cities</p>
-      <div className="grid grid-cols-2 p-4 gap-3 mobileL:grid-cols-4">
-        {data?.map((city) => (
-          <a key={city.id} href="/#">
-            {city.state_name}, {city.state_code}
-          </a>
-        ))}
-      </div>
+      <div className="grid grid-cols-2 p-4 gap-3 mobileL:grid-cols-4">{data?.map((city) => <CityLabel key={city.id} city={city} />)}</div>
     </div>
+  )
+}
+
+// Composing component
+type Props = {
+  city: MostSearchedCityType
+}
+
+function CityLabel({ city }: Props) {
+  const query = `city_id=${city.id}&state_id=${city.state_id}&distance=5000&location_based=yes&latitude=${city.latitude}&longitude=${city.longitude}&type=Most Searched Cities`
+  return (
+    <Link prefetch={false} href={`/search?${query}`}>
+      {city.state_name}, {city.state_code}
+    </Link>
   )
 }
