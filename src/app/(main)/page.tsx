2c41a323932c4c9aa22d35c3787acfd8606c29e2
reggiegunawan <reggiegunawan88@gmail.com>
2024-08-12 11:30:11 +0700

    build(error): try to hotfix

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,6 @@
 import { getHomepageDataset } from '../_lib/services/landing-page/homepage-dataset'
-// import { ArticleSection } from './components/article'
+import { ArticleSection } from './components/article'
+import { FeaturedUrologists } from './components/featured-urologists'
 import { FindUrologist } from './components/find-urologist'
 import { MostSearchedCities } from './components/most-searched-cities'
 import { PromotionBanner } from './components/promotion-banner'
@@ -7,16 +8,14 @@ import { PromotionBanner } from './components/promotion-banner'
 export default async function Home() {
   const data = await getHomepageDataset()
 
-  // console.log(data);
-
   return (
     <div className="flex flex-col gap-y-10">
       <div className="flex flex-col gap-y-16">
         <FindUrologist />
-        {/* <FeaturedUrologists featured_urologist={data.featured} highest_rated_urologist={data.highest_rated} /> */}
+        <FeaturedUrologists featured_urologist={data.featured} highest_rated_urologist={data.highest_rated} />
       </div>
       <PromotionBanner />
-      {/* <ArticleSection articles={data.articles} /> */}
+      <ArticleSection articles={data.articles} />
       <MostSearchedCities />
     </div>
   )
