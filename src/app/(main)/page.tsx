af47b813321ce66db2582aa488b2f430c0f682a8
reggiegunawan <reggiegunawan88@gmail.com>
2024-08-12 10:36:09 +0700

    test: test remove homepage fetch

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,17 +1,17 @@
-import { getHomepageDataset } from '../_lib/services/landing-page/homepage-dataset'
-import { FeaturedUrologists } from './components/featured-urologists'
+// import { getHomepageDataset } from '../_lib/services/landing-page/homepage-dataset'
+// import { FeaturedUrologists } from './components/featured-urologists'
 import { FindUrologist } from './components/find-urologist'
 import { MostSearchedCities } from './components/most-searched-cities'
 import { PromotionBanner } from './components/promotion-banner'
 
 export default async function Home() {
-  const data = await getHomepageDataset()
+  // const data = await getHomepageDataset()
 
   return (
     <div className="flex flex-col gap-y-10">
       <div className="flex flex-col gap-y-16">
         <FindUrologist />
-        <FeaturedUrologists featured_urologist={data.featured} highest_rated_urologist={data.highest_rated} />
+        {/* <FeaturedUrologists featured_urologist={data.featured} highest_rated_urologist={data.highest_rated} /> */}
       </div>
       <PromotionBanner />
       {/* <ArticleSection articles={data.articles} /> */}
