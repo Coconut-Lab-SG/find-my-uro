6397702066e0524c931a63462fbd800ceeba363d
reggiegunawan <reggiegunawan88@gmail.com>
2024-08-12 10:41:22 +0700

    test(build): re-add featured uro section

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,6 @@
 import { getHomepageDataset } from '../_lib/services/landing-page/homepage-dataset'
-import { ArticleSection } from './components/article'
-// import { FeaturedUrologists } from './components/featured-urologists'
+// import { ArticleSection } from './components/article'
+import { FeaturedUrologists } from './components/featured-urologists'
 import { FindUrologist } from './components/find-urologist'
 import { MostSearchedCities } from './components/most-searched-cities'
 import { PromotionBanner } from './components/promotion-banner'
@@ -12,10 +12,10 @@ export default async function Home() {
     <div className="flex flex-col gap-y-10">
       <div className="flex flex-col gap-y-16">
         <FindUrologist />
-        {/* <FeaturedUrologists featured_urologist={data.featured} highest_rated_urologist={data.highest_rated} /> */}
+        <FeaturedUrologists featured_urologist={data.featured} highest_rated_urologist={data.highest_rated} />
       </div>
       <PromotionBanner />
-      <ArticleSection articles={data.articles} />
+      {/* <ArticleSection articles={data.articles} /> */}
       <MostSearchedCities />
     </div>
   )
