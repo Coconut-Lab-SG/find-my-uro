35b8291df48631ebd6e58acbc4d894f07d6ab7f6
reggiegunawan <reggiegunawan88@gmail.com>
2024-08-15 20:36:25 +0700

    feat(urologist): add more padding for featured uro label

--- src/app/_components/featured-urologist-label.tsx
+++ src/app/_components/featured-urologist-label.tsx
@@ -2,7 +2,7 @@ import { Star } from 'lucide-react'
 
 export function FeaturedUrologistLabel() {
   return (
-    <div className="flex gap-1 items-center bg-[#FFB800] text-white text-xs rounded-lg p-[5px] w-fit">
+    <div className="flex gap-1 items-center bg-[#FFB800] text-white text-xs rounded-lg py-1 px-2 w-fit">
       <Star size="12" fill="#fff" strokeWidth={0} />
       <span>Featured</span>
     </div>
